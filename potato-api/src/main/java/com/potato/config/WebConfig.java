1e9d5f8bad0afc8c97a22bde296aca83677e046d
seungh0 <will.seungho@gmail.com>
2021-02-14 20:34:02 +0900

    feat(README): Create README.md

--- potato-api/src/main/java/com/potato/config/WebConfig.java
+++ potato-api/src/main/java/com/potato/config/WebConfig.java
@@ -7,11 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOrigins("*")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS");
-	}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS");
+    }
 
 }
