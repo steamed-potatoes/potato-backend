77da7c4d3b21a63c767d7e62bec06640ed59c9cb
seungh0 <will.seungho@gmail.com>
2021-02-15 11:46:56 +0900

    feat(google): GET -> POST 변경

--- potato-api/src/main/java/com/potato/controller/auth/AuthController.java
+++ potato-api/src/main/java/com/potato/controller/auth/AuthController.java
@@ -5,7 +5,8 @@ import com.potato.service.auth.GoogleAuthService;
 import com.potato.service.auth.dto.request.AuthRequest;
 import com.potato.service.auth.dto.response.AuthResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -16,8 +17,8 @@ public class AuthController {
 
     private final GoogleAuthService googleAuthService;
 
-    @GetMapping("/api/v1/auth/google")
-    public ApiResponse<AuthResponse> handleGoogleAuthentication(@Valid AuthRequest request) {
+    @PostMapping("/api/v1/auth/google")
+    public ApiResponse<AuthResponse> handleGoogleAuthentication(@Valid @RequestBody AuthRequest request) {
         return ApiResponse.of(googleAuthService.handleGoogleAuthentication(request));
     }
 
