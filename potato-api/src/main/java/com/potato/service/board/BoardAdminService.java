86f9aa132953a5bb66387a4af7f83b92094d5811
yusunjo <tnswh2023@gmail.com>
2021-02-26 17:10:09 +0900

    feat(board): createBoard

---
+++ potato-api/src/main/java/com/potato/service/board/BoardAdminService.java
@@ -0,0 +1,29 @@
+package com.potato.service.board;
+
+import com.potato.domain.board.Board;
+import com.potato.domain.board.BoardRepository;
+import com.potato.domain.organization.Organization;
+import com.potato.domain.organization.OrganizationRepository;
+import com.potato.service.board.dto.request.CreateBoardRequest;
+import com.potato.service.board.dto.response.BoardInfoResponse;
+import com.potato.service.organization.OrganizationServiceUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class BoardAdminService {
+
+    private final BoardRepository boardRepository;
+    private final OrganizationRepository organizationRepository;
+
+    @Transactional
+    public BoardInfoResponse createBoard(String subDomain, CreateBoardRequest request, Long memberId) {
+        Organization organization = OrganizationServiceUtils.findOrganizationBySubDomain(organizationRepository, subDomain);
+        Board board = request.toEntity(organization.getId(), memberId);
+        boardRepository.save(board);
+        return BoardInfoResponse.of(board);
+    }
+
+}
