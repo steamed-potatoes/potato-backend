6fc28c11e3d146af7877c6be369504444e31f773
Seungho Kang <will.seungho@gmail.com>
2021-03-05 00:40:42 +0900

    feat: 삭제된 댓글의 경우 삭제된 메시지라고 표시하도록 설정 (#28)

--- potato-api/src/main/java/com/potato/service/comment/dto/response/BoardCommentResponse.java
+++ potato-api/src/main/java/com/potato/service/comment/dto/response/BoardCommentResponse.java
@@ -1,6 +1,7 @@
 package com.potato.service.comment.dto.response;
 
 import com.potato.domain.comment.BoardComment;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
@@ -11,7 +12,7 @@ import java.util.stream.Collectors;
 
 @ToString
 @Getter
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class BoardCommentResponse {
 
     private final Long id;
@@ -25,13 +26,27 @@ public class BoardCommentResponse {
     private final List<BoardCommentResponse> children = new ArrayList<>();
 
     public static BoardCommentResponse of(BoardComment comment) {
-        BoardCommentResponse response = new BoardCommentResponse(comment.getId(), comment.getBoardId(),
-            comment.getMemberId(), comment.getContent());
+        BoardCommentResponse response = activeOrInActiveComment(comment);
         List<BoardCommentResponse> childResponses = comment.getChildComments().stream()
             .map(BoardCommentResponse::of)
             .collect(Collectors.toList());
         response.children.addAll(childResponses);
         return response;
+    }
+
+    private static BoardCommentResponse activeOrInActiveComment(BoardComment comment) {
+        if (comment.isDeleted()) {
+            return inActiveComment(comment);
+        }
+        return activeComment(comment);
+    }
+
+    private static BoardCommentResponse inActiveComment(BoardComment comment) {
+        return new BoardCommentResponse(comment.getId(), comment.getBoardId(), null, "삭제된 메시지입니다");
+    }
+
+    private static BoardCommentResponse activeComment(BoardComment comment) {
+        return new BoardCommentResponse(comment.getId(), comment.getBoardId(), comment.getMemberId(), comment.getContent());
     }
 
 }
