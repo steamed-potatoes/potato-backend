84c3d5f0f28277e1f9844f94d414a1709ceec2b2
seungh0 <will.seungho@gmail.com>
2021-03-15 10:59:26 +0900

    feat: v2로 변경 후, TODO 작성

--- potato-api/src/main/java/com/potato/controller/boardv2/OrganizationBoardController.java
+++ potato-api/src/main/java/com/potato/controller/boardv2/OrganizationBoardController.java
@@ -21,16 +21,26 @@ public class OrganizationBoardController {
 
     @Operation(summary = "그룹의 관리자가 새로운 그룹의 게시물을 등록하는 API", description = "Bearer 토큰이 필요합니다")
     @Auth
-    @PostMapping("/api/v1/organization/board/{subDomain}")
+    @PostMapping("/api/v2/organization/board/{subDomain}")
     public ApiResponse<OrganizationBoardInfoResponse> createOrganizationBoard(
         @PathVariable String subDomain, @Valid @RequestBody CreateOrganizationBoardRequest request, @MemberId Long memberId) {
         return ApiResponse.of(organizationBoardService.createBoard(subDomain, request, memberId));
     }
 
     @Operation(summary = "특정 그룹의 게시물을 조회하는 API")
-    @GetMapping("/api/v1/organization/board/{organizationBoardId}")
+    @GetMapping("/api/v2/organization/board/{organizationBoardId}")
     public ApiResponse<OrganizationBoardWithCreatorInfoResponse> retrieveOrganizationBoard(@PathVariable Long organizationBoardId) {
         return ApiResponse.of(organizationBoardService.retrieveBoard(organizationBoardId));
     }
+
+    // TODO: 그룹의 게시물을 스크롤 페이지네이션 기반으로 조회하는 API
+
+    // TODO: 그룹의 관리자가 그룹의 게시물을 수정하는 API
+
+    // TODO: 그룹의 관리자가 그룹의 게시물을 삭제하는 API
+
+    // TODO: 게시물의 좋아요를 추가하는 API
+
+    // TODO: 게시물의 좋아요를 취소하는 API
 
 }
