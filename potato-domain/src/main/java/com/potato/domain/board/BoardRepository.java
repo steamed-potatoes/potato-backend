2f0c0b199109844ed2218593fda139e3d93cb828
seungh0 <will.seungho@gmail.com>
2021-03-15 12:52:56 +0900

    fix: 그룹 게시물 관련 v1 삭제

--- potato-domain/src/main/java/com/potato/domain/board/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.potato.domain.board;
-
-import com.potato.domain.board.repository.BoardRepositoryCustom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long>, BoardRepositoryCustom {
-}
