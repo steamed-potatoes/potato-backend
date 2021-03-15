effab4bd99a5e9c37c8195faf3f6122172b692a5
seungh0 <will.seungho@gmail.com>
2021-03-15 10:52:22 +0900

    feat: 그룹 게시물을 상세 조회시 그룹 정보와 작성자 정보도 같이 반환

--- potato-api/src/main/java/com/potato/controller/boardv2/OrganizationBoardController.java
+++ potato-api/src/main/java/com/potato/controller/boardv2/OrganizationBoardController.java
@@ -6,6 +6,8 @@ import com.potato.controller.ApiResponse;
 import com.potato.service.boardv2.OrganizationBoardService;
 import com.potato.service.boardv2.dto.request.CreateOrganizationBoardRequest;
 import com.potato.service.boardv2.dto.response.OrganizationBoardInfoResponse;
+import com.potato.service.boardv2.dto.response.OrganizationBoardWithCreatorInfoResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,17 +19,17 @@ public class OrganizationBoardController {
 
     private final OrganizationBoardService organizationBoardService;
 
+    @Operation(summary = "그룹의 관리자가 새로운 그룹의 게시물을 등록하는 API", description = "Bearer 토큰이 필요합니다")
     @Auth
     @PostMapping("/api/v1/organization/board/{subDomain}")
-    public ApiResponse<String> createOrganizationBoard(@PathVariable String subDomain,
-                                                       @Valid @RequestBody CreateOrganizationBoardRequest request,
-                                                       @MemberId Long memberId) {
-        organizationBoardService.createBoard(subDomain, request, memberId);
-        return ApiResponse.OK;
+    public ApiResponse<OrganizationBoardInfoResponse> createOrganizationBoard(
+        @PathVariable String subDomain, @Valid @RequestBody CreateOrganizationBoardRequest request, @MemberId Long memberId) {
+        return ApiResponse.of(organizationBoardService.createBoard(subDomain, request, memberId));
     }
 
+    @Operation(summary = "특정 그룹의 게시물을 조회하는 API")
     @GetMapping("/api/v1/organization/board/{organizationBoardId}")
-    public ApiResponse<OrganizationBoardInfoResponse> retrieveOrganizationBoard(@PathVariable Long organizationBoardId) {
+    public ApiResponse<OrganizationBoardWithCreatorInfoResponse> retrieveOrganizationBoard(@PathVariable Long organizationBoardId) {
         return ApiResponse.of(organizationBoardService.retrieveBoard(organizationBoardId));
     }
 
