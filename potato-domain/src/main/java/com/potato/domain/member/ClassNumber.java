69a9d49953f1da54386accce7a66824a1e6aee14
Seungho Kang <will.seungho@gmail.com>
2021-03-23 00:13:32 +0900

    refactor: 학번 유효범위 지정 (#55)

--- potato-domain/src/main/java/com/potato/domain/member/ClassNumber.java
+++ potato-domain/src/main/java/com/potato/domain/member/ClassNumber.java
@@ -7,31 +7,47 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class ClassNumber {
 
+    private final static int DIGIT = 100_000;
+    private final static int MIN_YEAR = 1953;
+
     @Column(nullable = false, length = 9)
     private Integer classNumber;
 
     private ClassNumber(Integer classNumber) {
-        validateIsPositive(classNumber);
-        validateIsNineLength(classNumber);
+        validateClassNumberFormat(classNumber);
         this.classNumber = classNumber;
     }
 
-    private void validateIsPositive(Integer classNumber) {
-        if (classNumber < 0) {
-            throw new ValidationException(String.format("(%d) 학번은 양수가 아닙니다.", classNumber), "학번이 양수가 아닙니다.");
+    private void validateClassNumberFormat(Integer classNumber) {
+        validateClassNumberRange(classNumber);
+        validateClassNumberLength(classNumber);
+    }
+
+    private void validateClassNumberRange(Integer classNumber) {
+        if (classNumber < getMinClassNumber() || classNumber >= getMaxClassNumber()) {
+            throw new ValidationException(String.format("학번 (%d)은 잘못되었습니다.", classNumber), "잘못된 학번입니다.");
         }
     }
 
-    private void validateIsNineLength(Integer classNumber) {
-        int length = (int) (Math.log10(classNumber) + 1);
-        if (length != 9) {
-            throw new ValidationException(String.format("(%d) 학번은 9자리가 아닙니다", classNumber), "학번이 9자리수가 아닙니다.");
+    private int getMinClassNumber() {
+        return MIN_YEAR * DIGIT;
+    }
+
+    private int getMaxClassNumber() {
+        final int currentYear = LocalDateTime.now().plusYears(1).getYear();
+        return currentYear * DIGIT;
+    }
+
+    private void validateClassNumberLength(Integer classNumber) {
+        if (String.valueOf(classNumber).length() != 9) {
+            throw new ValidationException(String.format("(%d) 학번은 9자리가 아닙니다", classNumber), "잘못된 학번입니다.");
         }
     }
 
--- potato-domain/src/test/java/com/potato/domain/member/ClassNumberTest.java
+++ potato-domain/src/test/java/com/potato/domain/member/ClassNumberTest.java
@@ -1,12 +1,11 @@
 package com.potato.domain.member;
 
 import com.potato.exception.ValidationException;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class ClassNumberTest {
+class ClassNumberTest {
 
     @Test
     void 정상적인_아홉자리와_양수인_학번() {
@@ -22,24 +21,58 @@ public class ClassNumberTest {
 
     @Test
     void 학번이_양수가_아닐경우() {
-        //given
+        // given
         Integer classNumber = -201610323;
 
         // when & then
         assertThatThrownBy(
             () -> ClassNumber.of(classNumber)
-        ).isInstanceOf(ValidationException.class).hasMessage(String.format("(%d) 학번은 양수가 아닙니다.", classNumber));
+        ).isInstanceOf(ValidationException.class).hasMessage(String.format("학번 (%d)은 잘못되었습니다.", classNumber));
     }
 
     @Test
     void 학번이_아홉자리가_아닐경우() {
-        //given
+        // given
         Integer classNumber = 1234;
 
         // when & then
         assertThatThrownBy(
             () -> ClassNumber.of(classNumber)
         ).isInstanceOf(ValidationException.class);
+    }
+
+    @Test
+    void 유효하지_않은_학번일_경우_너무_옛날() {
+        // given
+        Integer classNumber = 195299999;
+
+        // when & then
+        assertThatThrownBy(
+            () -> ClassNumber.of(classNumber)
+        ).isInstanceOf(ValidationException.class);
+    }
+
+    @Test
+    void 미래의_학번() {
+        // given
+        Integer classNumber = 2030000000;
+
+        // when & then
+        assertThatThrownBy(
+            () -> ClassNumber.of(classNumber)
+        ).isInstanceOf(ValidationException.class);
+    }
+
+    @Test
+    void 유효한_학번일_경우() {
+        //given
+        Integer classNumber = 202110302;
+
+        // when
+        ClassNumber classNumber1 = ClassNumber.of(classNumber);
+
+        // then
+        assertThat(classNumber1.getClassNumber()).isEqualTo(classNumber);
     }
 
 }
