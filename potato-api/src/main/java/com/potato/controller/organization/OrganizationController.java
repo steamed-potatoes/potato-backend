f4093657d047b059a697ddafd49fa89be71dd592
seungh0 <will.seungho@gmail.com>
2021-03-23 22:37:35 +0900

    refactor: OrganizationRetrieveController 통합

--- potato-api/src/main/java/com/potato/controller/organization/OrganizationController.java
+++ potato-api/src/main/java/com/potato/controller/organization/OrganizationController.java
@@ -3,14 +3,19 @@ package com.potato.controller.organization;
 import com.potato.config.argumentResolver.MemberId;
 import com.potato.config.interceptor.auth.Auth;
 import com.potato.controller.ApiResponse;
+import com.potato.service.member.dto.response.MemberInfoResponse;
+import com.potato.service.organization.OrganizationRetrieveService;
 import com.potato.service.organization.OrganizationService;
 import com.potato.service.organization.dto.request.CreateOrganizationRequest;
 import com.potato.service.organization.dto.response.OrganizationInfoResponse;
+import com.potato.service.organization.dto.response.OrganizationWithMembersInfoResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import java.util.List;
 
 import static com.potato.config.interceptor.auth.Auth.Role.ORGANIZATION_MEMBER;
 
@@ -19,12 +24,32 @@ import static com.potato.config.interceptor.auth.Auth.Role.ORGANIZATION_MEMBER;
 public class OrganizationController {
 
     private final OrganizationService organizationService;
+    private final OrganizationRetrieveService organizationRetrieveService;
 
     @Operation(summary = "새로운 그룹을 생성하는 API", description = "Bearer 토큰이 필요합니다")
     @Auth
     @PostMapping("/api/v1/organization")
     public ApiResponse<OrganizationInfoResponse> createOrganization(@Valid @RequestBody CreateOrganizationRequest request, @MemberId Long memberId) {
         return ApiResponse.success(organizationService.createOrganization(request, memberId));
+    }
+
+    @Operation(summary = "특정 그룹의 정보를 조회하는 API", description = "해당하는 그룹의 subDomain")
+    @GetMapping("/api/v1/organization/{subDomain}")
+    public ApiResponse<OrganizationWithMembersInfoResponse> getDetailOrganizationInfo(@PathVariable String subDomain) {
+        return ApiResponse.success(organizationRetrieveService.getDetailOrganizationInfo(subDomain));
+    }
+
+    @Operation(summary = "등록된 그룹 리스트를 불러오는 API")
+    @GetMapping("/api/v1/organization/list")
+    public ApiResponse<List<OrganizationInfoResponse>> getOrganizations() {
+        return ApiResponse.success(organizationRetrieveService.getOrganizationsInfo());
+    }
+
+    @Operation(summary = "내가 속한 그룹의 리스트를 불러오는 API", description = "Bearer 토큰이 필요합니다")
+    @Auth
+    @GetMapping("/api/v1/organization/my")
+    public ApiResponse<List<OrganizationInfoResponse>> getMyOrganizations(@MemberId Long memberId) {
+        return ApiResponse.success(organizationRetrieveService.getMyOrganizationsInfo(memberId));
     }
 
     @Operation(summary = "특정 그룹에 가입신청을 하는 API", description = "Bearer 토큰이 필요합니다")
@@ -65,6 +90,12 @@ public class OrganizationController {
     public ApiResponse<String> unFollowOrganization(@PathVariable String subDomain, @MemberId Long memberId) {
         organizationService.unFollowOrganization(subDomain, memberId);
         return ApiResponse.OK;
+    }
+
+    @Operation(summary = "그룹을 팔로우한 멤버 리스트를 불러오는 API")
+    @GetMapping("/api/v1/organization/follow/{subDomain}")
+    public ApiResponse<List<MemberInfoResponse>> getOrganizationFollowMember(@PathVariable String subDomain) {
+        return ApiResponse.success(organizationRetrieveService.getOrganizationFollowMember(subDomain));
     }
 
 }
