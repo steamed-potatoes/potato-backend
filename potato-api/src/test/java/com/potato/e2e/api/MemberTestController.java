35352427156d607a17e94e3d3e1bc2a512c260d2
seungh0 <will.seungho@gmail.com>
2021-03-26 18:30:01 +0900

    feat: MemberController Test

---
+++ potato-api/src/test/java/com/potato/e2e/api/MemberTestController.java
@@ -0,0 +1,95 @@
+package com.potato.e2e.api;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.potato.controller.ApiResponse;
+import com.potato.domain.member.MemberMajor;
+import com.potato.service.member.dto.request.CreateMemberRequest;
+import com.potato.service.member.dto.request.UpdateMemberRequest;
+import com.potato.service.member.dto.response.MemberInfoResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+public class MemberTestController {
+
+    private final MockMvc mockMvc;
+    private final ObjectMapper objectMapper;
+
+    public MemberTestController(MockMvc mockMvc, ObjectMapper objectMapper) {
+        this.mockMvc = mockMvc;
+        this.objectMapper = objectMapper;
+    }
+
+    public ApiResponse<String> createMember(CreateMemberRequest request) throws Exception {
+        MockHttpServletRequestBuilder builder = post("/api/v1/member")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request));
+
+        return objectMapper.readValue(
+            mockMvc.perform(builder)
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {
+            }
+        );
+    }
+
+    public ApiResponse<MemberInfoResponse> getMyMemberInfo(String token) throws Exception {
+        MockHttpServletRequestBuilder builder = get("/api/v1/member")
+            .header(HttpHeaders.AUTHORIZATION, "Bearer ".concat(token));
+
+        return objectMapper.readValue(
+            mockMvc.perform(builder)
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {
+            }
+        );
+    }
+
+    public ApiResponse<MemberInfoResponse> updateMemberInfo(UpdateMemberRequest request, String token) throws Exception {
+        MockHttpServletRequestBuilder builder = put("/api/v1/member")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request))
+            .header(HttpHeaders.AUTHORIZATION, "Bearer ".concat(token));
+
+        return objectMapper.readValue(
+            mockMvc.perform(builder)
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {
+            }
+        );
+    }
+
+    public ApiResponse<MemberInfoResponse> getMemberInfo(Long memberId, String token) throws Exception {
+        MockHttpServletRequestBuilder builder = get("/api/v1/member/" + memberId)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer ".concat(token));
+
+        return objectMapper.readValue(
+            mockMvc.perform(builder)
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {
+            }
+        );
+    }
+
+    public String getMockMemberToken() throws Exception {
+        CreateMemberRequest request = CreateMemberRequest.testBuilder()
+            .email("will.seungho@gmail.com")
+            .name("강승호")
+            .major(MemberMajor.IT_ICT)
+            .classNumber(201610302)
+            .profileUrl("http://profile.com")
+            .build();
+        return createMember(request).getData();
+    }
+
+}
--- potato-api/src/test/java/com/potato/e2e/member/MemberControllerTest.java
+++ potato-api/src/test/java/com/potato/e2e/member/MemberControllerTest.java
@@ -1,29 +1,33 @@
 package com.potato.e2e.member;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.potato.controller.member.MemberController;
-import com.potato.e2e.AbstractControllerTest;
+import com.potato.controller.ApiResponse;
+import com.potato.controller.advice.ErrorCode;
 import com.potato.domain.member.Member;
+import com.potato.domain.member.MemberCreator;
+import com.potato.domain.member.MemberMajor;
 import com.potato.domain.member.MemberRepository;
+import com.potato.e2e.api.MemberTestController;
 import com.potato.service.member.dto.request.CreateMemberRequest;
-import org.junit.jupiter.api.AfterEach;
+import com.potato.service.member.dto.request.UpdateMemberRequest;
+import com.potato.service.member.dto.response.MemberInfoResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-
-import java.util.List;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureMockMvc
 @SpringBootTest
-class MemberControllerTest extends AbstractControllerTest {
+class MemberControllerTest {
 
     @Autowired
-    private MemberController memberController;
+    private MockMvc mockMvc;
+
+    private MemberTestController memberTestController;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -31,60 +35,161 @@ class MemberControllerTest extends AbstractControllerTest {
     @Autowired
     private MemberRepository memberRepository;
 
-    @Override
-    protected Object controller() {
-        return memberController;
-    }
-
-    @AfterEach
+    @BeforeEach
     void cleanUp() {
         memberRepository.deleteAll();
     }
 
-    @Test
-    void 회원가입_API_호출시_200OK() throws Exception {
-        // given
-        String email = "will.seungho@gmail.com";
-        String name = "강승호";
-        Integer classNumber = 201610323;
-
-        CreateMemberRequest request = CreateMemberRequest.testBuilder()
-            .email(email)
-            .name(name)
-            .classNumber(classNumber)
-            .build();
-
-        // when
-        this.mockMvc.perform(post("/api/v1/member")
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .content(objectMapper.writeValueAsBytes(request)))
-            .andDo(print())
-            .andExpect(status().isOk());
-
-        // then
-        List<Member> memberList = memberRepository.findAll();
-        assertThat(memberList).hasSize(1);
+    @BeforeEach
+    void setUp() {
+        memberTestController = new MemberTestController(mockMvc, objectMapper);
     }
 
     @Test
-    void 회원가입_API_이메일을_입력하지_않을경우_400_ERROR() throws Exception {
+    void 회원가입_API_호출시_생성된_토큰이_반환된다() throws Exception {
         // given
-        String name = "강승호";
-
         CreateMemberRequest request = CreateMemberRequest.testBuilder()
-            .name(name)
+            .email("test.seungho@gmail.com")
+            .name("강승호")
+            .major(MemberMajor.IT_ICT)
+            .classNumber(201610302)
+            .profileUrl("http://profile.com")
             .build();
 
         // when
-        this.mockMvc.perform(post("/api/v1/member")
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .content(objectMapper.writeValueAsBytes(request)))
-            .andDo(print())
-            .andExpect(status().isBadRequest());
+        ApiResponse<String> response = memberTestController.createMember(request);
 
         // then
-        List<Member> memberList = memberRepository.findAll();
-        assertThat(memberList).isEmpty();
+        assertThat(response.getData()).isNotNull();
+    }
+
+    @Test
+    void 회원가입_API_호출시_이미_존재하는_이메일의경우_409_에러() throws Exception {
+        // given
+        memberRepository.save(MemberCreator.create("test.seungho@gmail.com"));
+
+        CreateMemberRequest request = CreateMemberRequest.testBuilder()
+            .email("test.seungho@gmail.com")
+            .name("강승호")
+            .major(MemberMajor.IT_ICT)
+            .classNumber(201610302)
+            .profileUrl("http://profile.com")
+            .build();
+
+        // when
+        ApiResponse<String> response = memberTestController.createMember(request);
+
+        // then
+        assertThat(response.getCode()).isEqualTo(ErrorCode.CONFLICT_EXCEPTION.getCode());
+    }
+
+    @Test
+    void 회원가입_API_호출시_이메일을_입력하지_않으면_400_에러() throws Exception {
+        // given
+        memberRepository.save(MemberCreator.create("will.seungho@gmail.com"));
+
+        CreateMemberRequest request = CreateMemberRequest.testBuilder()
+            .name("강승호")
+            .major(MemberMajor.IT_ICT)
+            .classNumber(201610302)
+            .profileUrl("http://profile.com")
+            .build();
+
+        // when
+        ApiResponse<String> response = memberTestController.createMember(request);
+
+        // then
+        assertThat(response.getCode()).isEqualTo(ErrorCode.VALIDATION_EXCEPTION.getCode());
+    }
+
+    @Test
+    void 회원가입_API_호출시_이름을_입력하지_않으면_400_에러() throws Exception {
+        // given
+        memberRepository.save(MemberCreator.create("will.seungho@gmail.com"));
+
+        CreateMemberRequest request = CreateMemberRequest.testBuilder()
+            .email("will.seungho@gmail.com")
+            .major(MemberMajor.IT_ICT)
+            .classNumber(201610302)
+            .profileUrl("http://profile.com")
+            .build();
+
+        // when
+        ApiResponse<String> response = memberTestController.createMember(request);
+
+        // then
+        assertThat(response.getCode()).isEqualTo(ErrorCode.VALIDATION_EXCEPTION.getCode());
+    }
+
+    @Test
+    void 토큰을_이용해_내_정보를_불러온다() throws Exception {
+        // given
+        String token = memberTestController.getMockMemberToken();
+
+        // when
+        ApiResponse<MemberInfoResponse> response = memberTestController.getMyMemberInfo(token);
+
+        // then
+        assertThat(response.getData().getEmail()).isEqualTo("will.seungho@gmail.com");
+        assertThat(response.getData().getName()).isEqualTo("강승호");
+        assertThat(response.getData().getMajor()).isEqualTo("IT학부, ICT융합학과");
+        assertThat(response.getData().getClassNumber()).isEqualTo(201610302);
+        assertThat(response.getData().getProfileUrl()).isEqualTo("http://profile.com");
+    }
+
+    @Test
+    void 토큰을_이용해_내_정보를_불러올때_잘못된_토큰이면_401_에러가_발생() throws Exception {
+        // when
+        ApiResponse<MemberInfoResponse> response = memberTestController.getMyMemberInfo("wrong token");
+
+        // then
+        assertThat(response.getCode()).isEqualTo(ErrorCode.UNAUTHORIZED_EXCEPTION.getCode());
+    }
+
+    @Test
+    void 내정보를_수정한다() throws Exception {
+        // given
+        String name = "승호강";
+        String profileUrl = "http://abc.com";
+        MemberMajor major = MemberMajor.IT_COMPUTER;
+        Integer classNumber = 201710302;
+
+        UpdateMemberRequest request = UpdateMemberRequest.testBuilder()
+            .name(name)
+            .profileUrl(profileUrl)
+            .major(major)
+            .classNumber(classNumber)
+            .build();
+
+        String token = memberTestController.getMockMemberToken();
+
+        // when
+        ApiResponse<MemberInfoResponse> response = memberTestController.updateMemberInfo(request, token);
+
+        // then
+        assertThat(response.getData().getEmail()).isEqualTo("will.seungho@gmail.com");
+        assertThat(response.getData().getName()).isEqualTo(name);
+        assertThat(response.getData().getMajor()).isEqualTo(major.getName());
+        assertThat(response.getData().getClassNumber()).isEqualTo(classNumber);
+        assertThat(response.getData().getProfileUrl()).isEqualTo(profileUrl);
+    }
+
+    @Test
+    void 특정_유저의_정보를_불러온다() throws Exception {
+        // given
+        String token = memberTestController.getMockMemberToken();
+
+        Member member = memberRepository.save(MemberCreator.create("ksh980212@gmail.com"));
+
+        // when
+        ApiResponse<MemberInfoResponse> response = memberTestController.getMemberInfo(member.getId(), token);
+
+        // then
+        assertThat(response.getData().getEmail()).isEqualTo(member.getEmail());
+        assertThat(response.getData().getName()).isEqualTo(member.getName());
+        assertThat(response.getData().getMajor()).isEqualTo(member.getMajor().getName());
+        assertThat(response.getData().getClassNumber()).isEqualTo(member.getClassNumber());
+        assertThat(response.getData().getProfileUrl()).isEqualTo(member.getProfileUrl());
     }
 
 }
