b6e793abeca512ffdb5d8bee9c21aaa78ea2ffdc
ballde <67853463+YuSunjo@users.noreply.github.com>
2021-03-30 20:03:49 +0900

    feat(organizationAdmin): 컨트롤러 테스트 (#71)

---
+++ potato-api/src/test/java/com/potato/controller/organization/OrganizationAdminControllerTest.java
@@ -0,0 +1,64 @@
+package com.potato.controller.organization;
+
+import com.potato.controller.ApiResponse;
+import com.potato.controller.ControllerTestUtils;
+import com.potato.domain.member.Member;
+import com.potato.domain.organization.Organization;
+import com.potato.domain.organization.OrganizationCreator;
+import com.potato.domain.organization.OrganizationRepository;
+import com.potato.service.organization.dto.request.UpdateOrganizationInfoRequest;
+import com.potato.service.organization.dto.response.OrganizationInfoResponse;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class OrganizationAdminControllerTest extends ControllerTestUtils {
+
+    private OrganizationMockMvc organizationMockMvc;
+
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
+    @BeforeEach
+    void setUp() {
+        super.setup();
+        organizationMockMvc = new OrganizationMockMvc(mockMvc, objectMapper);
+    }
+
+    @AfterEach
+    void cleanUp() {
+        super.cleanup();
+        organizationRepository.deleteAll();
+    }
+
+    @Test
+    void 그룹_관리자가_그룹의_정보를_수정하는_경우() throws Exception {
+        //given
+        String email = "tnswh2023@gmail.com";
+        String token = memberMockMvc.getMockMemberToken(email);
+        Member member = memberRepository.findMemberByEmail(email);
+
+        String subDomain = "potato";
+        Organization organization = OrganizationCreator.create(subDomain);
+        organization.addAdmin(member.getId());
+        organizationRepository.save(organization);
+
+        String updateName = "감자화이팅";
+        UpdateOrganizationInfoRequest request = UpdateOrganizationInfoRequest.testBuilder()
+            .name(updateName)
+            .build();
+
+        //when
+        ApiResponse<OrganizationInfoResponse> response = organizationMockMvc.updateOrganizationInfo(subDomain, request, token, 200);
+
+        //then
+        Assertions.assertThat(response.getData().getName()).isEqualTo(updateName);
+    }
+
+}
