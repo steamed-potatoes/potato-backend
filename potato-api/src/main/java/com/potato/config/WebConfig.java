1a202a54b793f6012c3f5b6bb330283786c4d54f
seungh0 <will.seungho@gmail.com>
2021-04-05 15:23:39 +0900

    feat: cors allowHeaders

--- potato-api/src/main/java/com/potato/config/WebConfig.java
+++ potato-api/src/main/java/com/potato/config/WebConfig.java
@@ -22,7 +22,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS");
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+            .allowedHeaders("*");
     }
 
     @Override
