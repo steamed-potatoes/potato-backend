3b49dd4cf1cba4b566f7f1a8b56b6ceb556479cd
yusunjo <tnswh2023@gmail.com>
2021-04-06 17:05:01 +0900

    fix(board): 충돌 해결

--- potato-api/src/main/java/com/potato/config/WebConfig.java
+++ potato-api/src/main/java/com/potato/config/WebConfig.java
@@ -21,9 +21,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins(
+                "https://ourpotato.netlify.app", "http://ourpotato.netlify.app",
+                "https://potato-admin.netlify.app", "http://potato-admin.netlify.app",
+                "http://localhost:3000"
+            )
             .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-            .allowedHeaders("*");
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 
     @Override
