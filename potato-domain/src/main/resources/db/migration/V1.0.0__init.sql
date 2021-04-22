8b02b4ee690ca88a63eecec539621cd18d508598
seungh0 <will.seungho@gmail.com>
2021-04-23 01:24:23 +0900

    feat: Organization DDL

--- potato-domain/src/main/resources/db/migration/V1.0.0__init.sql
+++ potato-domain/src/main/resources/db/migration/V1.0.0__init.sql
@@ -47,3 +47,44 @@ CREATE TABLE IF NOT EXISTS `administrator`
     PRIMARY KEY (`id`),
     UNIQUE KEY `uni_administrator_1` (`email`)
 ) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `organization`
+(
+    `id`                      bigint      NOT NULL AUTO_INCREMENT,
+    `sub_domain`              varchar(50) NOT NULL,
+    `name`                    varchar(50) NOT NULL,
+    `category`                varchar(30) NOT NULL,
+    `profile_url`             varchar(2048)        DEFAULT NULL,
+    `description`             varchar(255)         DEFAULT NULL,
+    `members_count`           integer     NOT NULL DEFAULT 0,
+    `followers_count`         integer     NOT NULL DEFAULT 0,
+    `created_date_time`       datetime(6)          DEFAULT NULL,
+    `last_modified_date_time` datetime(6)          DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uni_organization_1` (`sub_domain`),
+    KEY `idx_organization_1` (`category`)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `organization_member_mapper`
+(
+    `id`                      bigint      NOT NULL AUTO_INCREMENT,
+    `organization_id`         bigint      NOT NULL,
+    `member_id`               bigint      NOT NULL,
+    `role`                    varchar(30) NOT NULL,
+    `created_date_time`       datetime(6) DEFAULT NULL,
+    `last_modified_date_time` datetime(6) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`organization_id`) REFERENCES `organization` (`id`),
+    KEY `idx_organization_member_mapper_1` (`member_id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `organization_follower`
+(
+    `id`                      bigint NOT NULL AUTO_INCREMENT,
+    `organization_id`         bigint NOT NULL,
+    `member_id`               bigint NOT NULL,
+    `created_date_time`       datetime(6) DEFAULT NULL,
+    `last_modified_date_time` datetime(6) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`organization_id`) REFERENCES `organization` (`id`)
+) ENGINE = InnoDB;
