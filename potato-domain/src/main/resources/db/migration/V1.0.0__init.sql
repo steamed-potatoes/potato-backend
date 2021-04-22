6e40ef23cb6ed6f6b418f33634dcf9c2fd326cc0
seungh0 <will.seungho@gmail.com>
2021-04-23 01:55:40 +0900

    fix: sql 컨벤션 수정

--- potato-domain/src/main/resources/db/migration/V1.0.0__init.sql
+++ potato-domain/src/main/resources/db/migration/V1.0.0__init.sql
@@ -1,37 +1,37 @@
-CREATE TABLE IF NOT EXISTS SPRING_SESSION
+CREATE TABLE IF NOT EXISTS `spring_session`
 (
-    SESSION_ID            CHAR(36) NOT NULL,
-    CREATION_TIME         BIGINT   NOT NULL,
-    LAST_ACCESS_TIME      BIGINT   NOT NULL,
-    MAX_INACTIVE_INTERVAL INT      NOT NULL,
-    PRINCIPAL_NAME        VARCHAR(100),
-    CONSTRAINT SPRING_SESSION_PK PRIMARY KEY (SESSION_ID)
+    `session_id`            CHAR(36) NOT NULL,
+    `creation_time`         BIGINT   NOT NULL,
+    `last_access_time`      BIGINT   NOT NULL,
+    `max_inactive_interval` INT      NOT NULL,
+    `principal_name`        VARCHAR(100),
+    CONSTRAINT `spring_session_pk` PRIMARY KEY (`session_id`)
 );
 
-CREATE INDEX IF NOT EXISTS SPRING_SESSION_IX1 ON SPRING_SESSION (LAST_ACCESS_TIME);
+CREATE INDEX IF NOT EXISTS `spring_session_ix1` ON `spring_session` (`last_access_time`);
 
-CREATE TABLE IF NOT EXISTS SPRING_SESSION_ATTRIBUTES
+CREATE TABLE IF NOT EXISTS `spring_session_attributes`
 (
-    SESSION_ID      CHAR(36)     NOT NULL,
-    ATTRIBUTE_NAME  VARCHAR(200) NOT NULL,
-    ATTRIBUTE_BYTES MEDIUMBLOB   NOT NULL,
-    CONSTRAINT SPRING_SESSION_ATTRIBUTES_PK PRIMARY KEY (SESSION_ID, ATTRIBUTE_NAME),
-    CONSTRAINT SPRING_SESSION_ATTRIBUTES_FK FOREIGN KEY (SESSION_ID) REFERENCES SPRING_SESSION (SESSION_ID) ON DELETE CASCADE
+    `session_id`      CHAR(36)     NOT NULL,
+    `attribute_name`  VARCHAR(200) NOT NULL,
+    `attribute_bytes` MEDIUMBLOB   NOT NULL,
+    CONSTRAINT SPRING_SESSION_ATTRIBUTES_PK PRIMARY KEY (`session_id`, `attribute_name`),
+    CONSTRAINT SPRING_SESSION_ATTRIBUTES_FK FOREIGN KEY (`session_id`) REFERENCES `spring_session` (`session_id`) ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS SPRING_SESSION_ATTRIBUTES_IX1 ON SPRING_SESSION_ATTRIBUTES (SESSION_ID);
+CREATE INDEX IF NOT EXISTS `spring_session_attributes_ix1` ON `spring_session_attributes` (`session_id`);
 
 CREATE TABLE IF NOT EXISTS `member`
 (
-    `id`                      bigint      NOT NULL AUTO_INCREMENT,
-    `email`                   varchar(50) NOT NULL,
-    `name`                    varchar(50) NOT NULL,
-    `profile_url`             varchar(2048) DEFAULT NULL,
-    `major`                   varchar(30)   DEFAULT NULL,
-    `class_number`            integer(9)    DEFAULT NULL,
-    `provider`                varchar(30) NOT NULL,
-    `created_date_time`       datetime(6)   DEFAULT NULL,
-    `last_modified_date_time` datetime(6)   DEFAULT NULL,
+    `id`                      BIGINT      NOT NULL AUTO_INCREMENT,
+    `email`                   VARCHAR(50) NOT NULL,
+    `name`                    VARCHAR(50) NOT NULL,
+    `profile_url`             VARCHAR(2048) DEFAULT NULL,
+    `major`                   VARCHAR(30)   DEFAULT NULL,
+    `class_number`            INTEGER(9)    DEFAULT NULL,
+    `provider`                VARCHAR(30) NOT NULL,
+    `created_date_time`       DATETIME(6)   DEFAULT NULL,
+    `last_modified_date_time` DATETIME(6)   DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `uni_member_1` (`email`)
 ) ENGINE = InnoDB;
@@ -39,27 +39,27 @@ CREATE TABLE IF NOT EXISTS `member`
 
 CREATE TABLE IF NOT EXISTS `administrator`
 (
-    `id`                      bigint      NOT NULL AUTO_INCREMENT,
-    `email`                   varchar(50) NOT NULL,
-    `name`                    varchar(50) NOT NULL,
-    `created_date_time`       datetime(6) DEFAULT NULL,
-    `last_modified_date_time` datetime(6) DEFAULT NULL,
+    `id`                      BIGINT      NOT NULL AUTO_INCREMENT,
+    `email`                   VARCHAR(50) NOT NULL,
+    `name`                    VARCHAR(50) NOT NULL,
+    `created_date_time`       DATETIME(6) DEFAULT NULL,
+    `last_modified_date_time` DATETIME(6) DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `uni_administrator_1` (`email`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `organization`
 (
-    `id`                      bigint      NOT NULL AUTO_INCREMENT,
-    `sub_domain`              varchar(50) NOT NULL,
-    `name`                    varchar(50) NOT NULL,
-    `category`                varchar(30) NOT NULL,
-    `profile_url`             varchar(2048)        DEFAULT NULL,
-    `description`             varchar(255)         DEFAULT NULL,
-    `members_count`           integer     NOT NULL DEFAULT 0,
-    `followers_count`         integer     NOT NULL DEFAULT 0,
-    `created_date_time`       datetime(6)          DEFAULT NULL,
-    `last_modified_date_time` datetime(6)          DEFAULT NULL,
+    `id`                      BIGINT      NOT NULL AUTO_INCREMENT,
+    `sub_domain`              VARCHAR(50) NOT NULL,
+    `name`                    VARCHAR(50) NOT NULL,
+    `category`                VARCHAR(30) NOT NULL,
+    `profile_url`             VARCHAR(2048)        DEFAULT NULL,
+    `description`             VARCHAR(255)         DEFAULT NULL,
+    `members_count`           INTEGER     NOT NULL DEFAULT 0,
+    `followers_count`         INTEGER     NOT NULL DEFAULT 0,
+    `created_date_time`       DATETIME(6)          DEFAULT NULL,
+    `last_modified_date_time` DATETIME(6)          DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `uni_organization_1` (`sub_domain`),
     KEY `idx_organization_1` (`category`)
@@ -67,12 +67,12 @@ CREATE TABLE IF NOT EXISTS `organization`
 
 CREATE TABLE IF NOT EXISTS `organization_member_mapper`
 (
-    `id`                      bigint      NOT NULL AUTO_INCREMENT,
-    `organization_id`         bigint      NOT NULL,
-    `member_id`               bigint      NOT NULL,
-    `role`                    varchar(30) NOT NULL,
-    `created_date_time`       datetime(6) DEFAULT NULL,
-    `last_modified_date_time` datetime(6) DEFAULT NULL,
+    `id`                      BIGINT      NOT NULL AUTO_INCREMENT,
+    `organization_id`         BIGINT      NOT NULL,
+    `member_id`               BIGINT      NOT NULL,
+    `role`                    VARCHAR(30) NOT NULL,
+    `created_date_time`       DATETIME(6) DEFAULT NULL,
+    `last_modified_date_time` DATETIME(6) DEFAULT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`organization_id`) REFERENCES `organization` (`id`),
     KEY `idx_organization_member_mapper_1` (`member_id`)
@@ -80,11 +80,11 @@ CREATE TABLE IF NOT EXISTS `organization_member_mapper`
 
 CREATE TABLE IF NOT EXISTS `organization_follower`
 (
-    `id`                      bigint NOT NULL AUTO_INCREMENT,
-    `organization_id`         bigint NOT NULL,
-    `member_id`               bigint NOT NULL,
-    `created_date_time`       datetime(6) DEFAULT NULL,
-    `last_modified_date_time` datetime(6) DEFAULT NULL,
+    `id`                      BIGINT NOT NULL AUTO_INCREMENT,
+    `organization_id`         BIGINT NOT NULL,
+    `member_id`               BIGINT NOT NULL,
+    `created_date_time`       DATETIME(6) DEFAULT NULL,
+    `last_modified_date_time` DATETIME(6) DEFAULT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`organization_id`) REFERENCES `organization` (`id`)
 ) ENGINE = InnoDB;
