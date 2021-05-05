3a154beb2dfc45e0b25328edc99298b08896ca88
Seungho Kang <will.seungho@gmail.com>
2021-05-06 00:49:21 +0900

    [POTATO-9] 블루그린 배포 잠시 유보

--- deploy/run.sh
+++ deploy/run.sh
@@ -4,25 +4,25 @@ DOCKER_APP_NAME=potato-api
 
 cat /GIT-TOKEN.txt | docker login ghcr.io -u USERNAME --password-stdin
 
-EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
 
-echo "레디스 Up"
-docker-compose up -d --build
-
-if [ -z "$EXIST_BLUE" ]; then
-    echo "Blue Up"
-    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
-
-    sleep 30
-
-    echo "Green Down"
-    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
-else
-    echo "Green Up"
-    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
-
-    sleep 30
-
-    echo "Blue Down"
-    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
-fi
+#EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+#
+#if [ -z "$EXIST_BLUE" ]; then
+#    echo "Blue Up"
+#    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+#
+#    sleep 30
+#
+#    echo "Green Down"
+#    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+#else
+#    echo "Green Up"
+#    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+#
+#    sleep 30
+#
+#    echo "Blue Down"
+#    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+#fi
