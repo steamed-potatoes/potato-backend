5225da8046de7e821c11c48551703075fcfc95b4
Seungho Kang <will.seungho@gmail.com>
2021-05-06 13:13:09 +0900

    [POTATO-9] 레디스도 블루 그린 배포 구성해서 다운타임 최소화

--- deploy/run.sh
+++ deploy/run.sh
@@ -4,25 +4,22 @@ DOCKER_APP_NAME=potato-api
 
 cat /GIT-TOKEN.txt | docker login ghcr.io -u USERNAME --password-stdin
 
-docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
-docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
-#EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
-#
-#if [ -z "$EXIST_BLUE" ]; then
-#    echo "Blue Up"
-#    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
-#
-#    sleep 30
-#
-#    echo "Green Down"
-#    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
-#else
-#    echo "Green Up"
-#    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
-#
-#    sleep 30
-#
-#    echo "Blue Down"
-#    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
-#fi
+if [ -z "$EXIST_BLUE" ]; then
+    echo "Blue Up"
+    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+
+    sleep 30
+
+    echo "Green Down"
+    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+else
+    echo "Green Up"
+    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+
+    sleep 30
+
+    echo "Blue Down"
+    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+fi
