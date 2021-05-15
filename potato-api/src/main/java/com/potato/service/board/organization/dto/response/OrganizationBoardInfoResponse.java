688a145ac4a9a2a36195b422156d7665b1ab65f4
Seungho Kang <will.seungho@gmail.com>
2021-05-15 12:06:21 +0900

    [POTATO-19] 게시물 상세 조회시 Auditing 정보 반환

--- potato-api/src/main/java/com/potato/service/board/organization/dto/response/OrganizationBoardInfoResponse.java
+++ potato-api/src/main/java/com/potato/service/board/organization/dto/response/OrganizationBoardInfoResponse.java
@@ -2,6 +2,7 @@ package com.potato.service.board.organization.dto.response;
 
 import com.potato.domain.board.organization.OrganizationBoard;
 import com.potato.domain.board.organization.OrganizationBoardCategory;
+import com.potato.service.common.dto.response.BaseTimeResponse;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -9,8 +10,7 @@ import java.time.LocalDateTime;
 @ToString
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class OrganizationBoardInfoResponse {
+public class OrganizationBoardInfoResponse extends BaseTimeResponse {
 
     private Long id;
 
@@ -30,10 +30,33 @@ public class OrganizationBoardInfoResponse {
 
     private OrganizationBoardCategory type;
 
+    @Builder
+    private OrganizationBoardInfoResponse(Long id, String subDomain, String title, LocalDateTime startDateTime, LocalDateTime endDateTime, String content, String imageUrl, int likesCount, OrganizationBoardCategory type) {
+        this.id = id;
+        this.subDomain = subDomain;
+        this.title = title;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+        this.content = content;
+        this.imageUrl = imageUrl;
+        this.likesCount = likesCount;
+        this.type = type;
+    }
+
     public static OrganizationBoardInfoResponse of(OrganizationBoard organizationBoard) {
-        return new OrganizationBoardInfoResponse(organizationBoard.getId(), organizationBoard.getSubDomain(), organizationBoard.getTitle(),
-            organizationBoard.getStartDateTime(), organizationBoard.getEndDateTime(), organizationBoard.getContent(),
-            organizationBoard.getImageUrl(), organizationBoard.getLikesCount(), organizationBoard.getCategory());
+        OrganizationBoardInfoResponse organizationBoardInfoResponse = OrganizationBoardInfoResponse.builder()
+            .id(organizationBoard.getId())
+            .subDomain(organizationBoard.getSubDomain())
+            .title(organizationBoard.getTitle())
+            .content(organizationBoard.getContent())
+            .startDateTime(organizationBoard.getStartDateTime())
+            .endDateTime(organizationBoard.getEndDateTime())
+            .imageUrl(organizationBoard.getImageUrl())
+            .likesCount(organizationBoard.getLikesCount())
+            .type(organizationBoard.getCategory())
+            .build();
+        organizationBoardInfoResponse.setBaseTime(organizationBoard);
+        return organizationBoardInfoResponse;
     }
 
 }
