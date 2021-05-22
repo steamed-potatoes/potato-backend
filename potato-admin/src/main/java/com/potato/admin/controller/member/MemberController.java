c6047c91b2545e4bee9e7751942a85716aace3ac
ballde <67853463+YuSunjo@users.noreply.github.com>
2021-05-22 15:38:28 +0900

    [POTATO-35]  adminControllerTest 보충 (#108)
    * [POTATO-35] adminControllerTest 보충
    
    * [POTATO-35] swagger security 추가

--- potato-admin/src/main/java/com/potato/admin/controller/member/MemberController.java
+++ potato-admin/src/main/java/com/potato/admin/controller/member/MemberController.java
@@ -4,6 +4,8 @@ import com.potato.admin.service.member.dto.response.MemberInfoResponse;
 import com.potato.admin.config.interceptor.Auth;
 import com.potato.admin.controller.ApiResponse;
 import com.potato.admin.service.member.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(summary = "가입되어 있는 멤버들 불러오기", security = {@SecurityRequirement(name = "BearerKey")})
     @Auth
     @GetMapping("/admin/v1/member/list")
     public ApiResponse<List<MemberInfoResponse>> retrieveMembersInfo() {
