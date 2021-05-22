e3c1ea4d90a410b8570b9ba5bfcc6c9c07d4ef18
YuSunjo <tnswh2023@gmail.com>
2021-05-22 15:24:37 +0900

    [POTATO-35] adminControllerTest 보충

--- potato-admin/src/test/java/com/potato/admin/controller/board/AdminBoardMockMvc.java
+++ potato-admin/src/test/java/com/potato/admin/controller/board/AdminBoardMockMvc.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.potato.admin.controller.ApiResponse;
 import com.potato.admin.service.board.dto.request.CreateAdminBoardRequest;
+import com.potato.admin.service.board.dto.request.DeleteOrganizationBoardRequest;
 import com.potato.admin.service.board.dto.request.UpdateAdminBoardRequest;
 import com.potato.admin.service.board.dto.response.AdminBoardInfoResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.nio.charset.StandardCharsets;
 
@@ -47,6 +50,23 @@ public class AdminBoardMockMvc {
             .contentType(MediaType.APPLICATION_JSON)
             .header(HttpHeaders.AUTHORIZATION, "Bearer ".concat(token))
             .content(objectMapper.writeValueAsString(request));
+
+        return objectMapper.readValue(
+            mockMvc.perform(builder)
+                .andExpect(status().is(expectedStatus))
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {
+            }
+        );
+    }
+
+    public ApiResponse<String> deleteOrganizationBoard(Long organizationBoardId, String subDomain, String token, int expectedStatus) throws Exception {
+        MultiValueMap<String, String> deleteBoardRequest = new LinkedMultiValueMap<>();
+        deleteBoardRequest.add("organizationBoardId", String.valueOf(organizationBoardId));
+        MockHttpServletRequestBuilder builder = delete("/admin/v1/board/organization/" + subDomain)
+            .params(deleteBoardRequest)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer ".concat(token));
 
         return objectMapper.readValue(
             mockMvc.perform(builder)
