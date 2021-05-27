925f2a49071eb8b42656fe65f284b3b63dc1c63d
Seungho Kang <will.seungho@gmail.com>
2021-05-27 21:56:56 +0900

    [POTATO-42] potato-common 모듈 테스트 커버리지 포함시키도록 수정

--- potato-api/src/main/java/com/potato/api/service/upload/request/FileUploadRequest.java
+++ potato-api/src/main/java/com/potato/api/service/upload/request/FileUploadRequest.java
@@ -2,22 +2,16 @@ package com.potato.api.service.upload.request;
 
 import com.potato.common.type.ImageType;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FileUploadRequest {
 
     @NotNull
     private ImageType type;
-
-    public static FileUploadRequest testInstance(ImageType type) {
-        return new FileUploadRequest(type);
-    }
 
 }
