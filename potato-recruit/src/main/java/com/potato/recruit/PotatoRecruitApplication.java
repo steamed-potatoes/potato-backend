e214205350fc7dfa91f0c97e3fa3218a2bdc6132
Seungho Kang <will.seungho@gmail.com>
2021-05-28 17:13:37 +0900

    [POTATO-RECRUIT-1] Potato Recruit 모듈 구성

---
+++ potato-recruit/src/main/java/com/potato/recruit/PotatoRecruitApplication.java
@@ -0,0 +1,13 @@
+package com.potato.recruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoRecruitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotatoRecruitApplication.class);
+    }
+
+}
